4c3d4f4022fb8b41f9c97bde4bfaed8d91dae31e
Stephen Radford <steve228uk@gmail.com>
2016-02-06 23:39:09 +0000

    Music message dict

--- MusicMessage.swift
+++ MusicMessage.swift
@@ -21,7 +21,10 @@ public class MusicMessage: Message {
     
     public var dictionary: [String:AnyObject] {
         get {
-            return [:]
+            return [
+                "type": "music",
+                "title": (title != nil) ? title! : ""
+            ]
         }
     }
     
